3d90cd8017bd7d12399a474d1494524fb8189cf5
Bojan Bogdanovic <bojan.bogdanovic@adera.no>
2024-09-22 13:36:24 +0200

    update grapql proxy

--- client/src/components/Cart/Cart.js
+++ client/src/components/Cart/Cart.js
@@ -1,123 +1,115 @@
-import React, { useContext, useState} from 'react';
+import React, { useContext, useState } from 'react';
 
 import "./Cart.css"
-import { graphql ,useMutation } from "react-apollo"
-import mutation from "../../mutations/addToHistory"
-// import mutation2 from "../../mutations/addOrderToOrders"
-import {  totalPrice }  from "../../utylity/totalPrice"
+import { useMutation } from "react-apollo"
+import { totalPrice } from "../../utylity/totalPrice"
 import { getLsInfo } from '../../utylity/getLsInfo';
-import {DataContext} from "../../context"
+import { DataContext } from "../../context"
 import CartItem from '../CartItem/CartItem';
 import CartUserFrom from './CartUserForm/CartUserFrom';
-import {cartValidation} from "../../utylity/cartValidation"
+import { cartValidation } from "../../utylity/cartValidation"
 import ValidationMessages from '../UI/Input/CartValidationMessages/ValidationMessages';
 import { addOrder } from '../../mutations/addOrderToOrders';
 const Cart = ({ mutate }) => {
-  const {setUser ,emptyCart , setOrders,orderss} = useContext(DataContext)
-  const {user, cart, auth} = getLsInfo();
-  const [addTodo, { data }] = useMutation(addOrder);   
-  let cartProducts = null; 
+  const { emptyCart, } = useContext(DataContext)
+  const { user, cart, auth } = getLsInfo();
+  const [addTodo, { data }] = useMutation(addOrder);
+  let cartProducts = null;
   let totalPriceOfProductsInCart = totalPrice(cart);
-  const [messages , setMessages] = useState([])
-  const [userData , setUserData] = useState({
-    firstName:"",
-    lastName:"",
-    email:"",
-    phone:"",
-    address:"",
+  const [messages, setMessages] = useState([])
+  const [userData, setUserData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    phone: "",
+    address: "",
   })
- const finishShoping = () => {  
-   addTodo({variables:{
-      id:new Date().toISOString(),
-      products:cart,
-   info:{
-          firstName:user.firstName,
-          lastName:user.lastName,
-          email:user.email,
-          phone:"3333-222",
-          address:user.street,
-      
-      
-        
+  const finishShoping = () => {
+    addTodo({
+      variables: {
+        id: new Date().toISOString(),
+        products: cart,
+        info: {
+          firstName: user.firstName,
+          lastName: user.lastName,
+          email: user.email,
+          phone: "3333-222",
+          address: user.street,
         },
-        total:""+totalPriceOfProductsInCart
-    }})
-    
-   console.log(data)
- 
+        total: "" + totalPriceOfProductsInCart
+      }
+    })
+    emptyCart()
+  }
 
-//  setUser({...user,history:[...user.history,...cart]})
-        emptyCart()
-    }
-
- const finishShopingInputs = e => {
-   setUserData({...userData,[e.target.name]:e.target.value})
- }   
-    const finishShopingNoUser = (e) => {  
+  const finishShopingInputs = e => {
+    setUserData({ ...userData, [e.target.name]: e.target.value })
+  }
+  const finishShopingNoUser = (e) => {
     e.preventDefault()
     console.log(userData)
     let valide = cartValidation(userData)
     let str = new Date().toISOString()
-    if(valide.valid){
+    if (valide.valid) {
       mutate({
-        variables:{
-          id:str,
-          products:cart,
-       info:{
-              firstName:userData.firstName,
-              lastName:userData.lastName,
-              email:userData.email,
-              phone:userData.phone,
-              address:userData.address,
-          
-            
-            }
-        }, 
-  })
- 
+        variables: {
+          id: str,
+          products: cart,
+          info: {
+            firstName: userData.firstName,
+            lastName: userData.lastName,
+            email: userData.email,
+            phone: userData.phone,
+            address: userData.address,
 
 
-        emptyCart()
+          }
+        },
+      })
+
+
+
+      emptyCart()
       setMessages([])
     }
-    else{
+    else {
       setMessages(valide.messages)
     }
 
   }
- 
 
 
-  if(cart.length > 0){
-    
+
+  if (cart.length > 0) {
+
     cartProducts = (
       <div>
-      { cart.map((product,i) => (   
-       <CartItem product={product} key={i} i={i}/>  ))}
+        {cart.map((product, i) => (
+          <CartItem product={product} key={i} i={i} />))}
       </div>
     )
 
   }
-    return (
-        <div>
-            {cartProducts}
-            {totalPriceOfProductsInCart !== 0 ? 
-            <h2 className="text-right price"> 
-              <span className="text-success">  Total :{totalPriceOfProductsInCart} din</span>
-            </h2> : null }
-            {!auth && cart.length > 0 ? <CartUserFrom  messages={messages}
-            finishShopingInputs={finishShopingInputs}
-            finishShoping={finishShopingNoUser}/> : null}
-            {cart.length > 0 && auth ? 
-            <button className = "btn btn-success" onClick = {finishShoping}>Finish Shoping</button>
-            : null}
-          
-            {(cart.length === 0 && auth) 
-            || (cart.length === 0 && !auth) 
-            ? <h2 className="text-center empty">
-            Emty Cart</h2>
-            :null}
-            </div>
-    );
+  return (
+    <div>
+      {cartProducts}
+      {totalPriceOfProductsInCart !== 0 ?
+        <h2 className="text-right price">
+          <span className="text-success">  Total :{totalPriceOfProductsInCart} din</span>
+        </h2> : null}
+      {!auth && cart.length > 0 ? <CartUserFrom messages={messages}
+        finishShopingInputs={finishShopingInputs}
+        finishShoping={finishShopingNoUser} /> : null}
+      {cart.length > 0 && auth ?
+        <button className="btn btn-success" onClick={finishShoping}>Finish Shoping</button>
+        : null}
+
+      {(cart.length === 0 && auth)
+        || (cart.length === 0 && !auth)
+        ? <h2 className="text-center empty">
+          Emty Cart</h2>
+        : null}
+    </div>
+  );
 }
 export default Cart;
--- client/src/context.js
+++ client/src/context.js
@@ -1,168 +1,164 @@
 
-import React,{createContext, useReducer} from "react"
-import  myReducer  from "./reducer"
+import React, { createContext, useReducer } from "react"
+import myReducer from "./reducer"
 
 const initialState = {
-    text : "hello world",
-    user:null,
-    auth:false,
-    orderss :[],
-    products:[
+    text: "hello world",
+    user: null,
+    auth: false,
+    orderss: [],
+    products: [
         {
-          name:"Lenovo 3360 S",
-          price:"30000",
-          matchName:"Lenovo3360S",
-          id:"1",
-          type:"home",
-          route:"televizori",
-          img_path:"lap.jpg",
-        quantity:0
-       ,
-    },
-        {
-         name:"Iphone 6s",
-         price:"60000",
-         type:"home",
-         route:"mobilni",
-         img_path:"mobile1.jpg",
-       quantity:0,
-         id:"2",
-                 
+            name: "Lenovo 3360 S",
+            price: "30000",
+            matchName: "Lenovo3360S",
+            id: "1",
+            type: "home",
+            route: "televizori",
+            img_path: "lap.jpg",
+            quantity: 0
+            ,
         },
         {
-            name:"DELL 360 A",
-            price:"50000",
-            id:"3",
-            type:"lap",
-            route:"laptopovi",
-            img_path:"lap1.jpg",
-          quantity:0 ,
-          
-        
-        }  ,
-        
-        {
-            name:"Toshiba 3333",
-            price:"30000",
-            id:"4",
-            type:"lap",
-            route:"laptopovi",
-            img_path:"lap2.jpg",
-          quantity:0,
-        
-        }  ,
-        {
-            name:"IPHONE 7S",
-            price:"50000",
-            id:"5",
-            type:"mob",
-            route:"mobilni",
-            img_path:"mobile2.jpg",
-          quantity:0,
-        
-            
-        }  ,
-        {
-            name:"SAMSUNG J5",
-            price:"20000",
-            id:"6",
-            type:"mob",
-            route:"mobilni",
-            img_path:"mobile3.jpg",
-          quantity:0,
-           
-        }  ,
-        {
-            name:"SAMSUNG TV",
-            price:"90000",
-            id:"7",
-            type:"tv",
-            route:"televizori",
-            img_path:"tv1.jpg",
-          quantity:0,
-           
-        }  ,
-        {
-            name:"SONY TV",
-            price:"70000",
-            id:"8",
-            type:"tv",
-            route:"televizori",
-            img_path:"tv3.jpg",
-          quantity:0,
-        
-        }         
-           
-    ],
-    shopingCart:localStorage.getItem("user") ? JSON.parse(localStorage.getItem('cart')) : []
+            name: "Iphone 6s",
+            price: "60000",
+            type: "home",
+            route: "mobilni",
+            img_path: "mobile1.jpg",
+            quantity: 0,
+            id: "2",
 
-} 
+        },
+        {
+            name: "DELL 360 A",
+            price: "50000",
+            id: "3",
+            type: "lap",
+            route: "laptopovi",
+            img_path: "lap1.jpg",
+            quantity: 0,
+
+
+        },
+
+        {
+            name: "Toshiba 3333",
+            price: "30000",
+            id: "4",
+            type: "lap",
+            route: "laptopovi",
+            img_path: "lap2.jpg",
+            quantity: 0,
+
+        },
+        {
+            name: "IPHONE 7S",
+            price: "50000",
+            id: "5",
+            type: "mob",
+            route: "mobilni",
+            img_path: "mobile2.jpg",
+            quantity: 0,
+
+
+        },
+        {
+            name: "SAMSUNG J5",
+            price: "20000",
+            id: "6",
+            type: "mob",
+            route: "mobilni",
+            img_path: "mobile3.jpg",
+            quantity: 0,
+
+        },
+        {
+            name: "SAMSUNG TV",
+            price: "90000",
+            id: "7",
+            type: "tv",
+            route: "televizori",
+            img_path: "tv1.jpg",
+            quantity: 0,
+
+        },
+        {
+            name: "SONY TV",
+            price: "70000",
+            id: "8",
+            type: "tv",
+            route: "televizori",
+            img_path: "tv3.jpg",
+            quantity: 0,
+
+        }
+
+    ],
+    shopingCart: localStorage.getItem("user") ? JSON.parse(localStorage.getItem('cart')) : []
+
+}
 
 export const DataContext = createContext(initialState)
 export const DataProvider = (props) => {
-    const [state , dispatch] = useReducer(myReducer, initialState)
-    // function setData (){
-    //     dispatch({type:"SET_DATA" , products:state.products})
-    // }
-    function setText () {
+    const [state, dispatch] = useReducer(myReducer, initialState)
+    function setText() {
         console.log(state)
         dispatch({
-            type:"SET_TEXT"
+            type: "SET_TEXT"
         })
     }
-    function setUserToLs () {
+    function setUserToLs() {
         dispatch({
-            type:"GET_USER_FROM_LS"
+            type: "GET_USER_FROM_LS"
         })
     }
-    function setUser(user){
+    function setUser(user) {
         dispatch({
-            type:"SET__USER",
-            payload:user
+            type: "SET__USER",
+            payload: user
         })
     }
 
-    
 
-    function setCartToLs (cart) {
+
+    function setCartToLs(cart) {
         dispatch({
-            type:"UPDATE__ITEM__IN__CART",
-            payload:cart
+            type: "UPDATE__ITEM__IN__CART",
+            payload: cart
         })
     }
     function emptyCart() {
         dispatch({
-            type:"EMPTY_CART"
+            type: "EMPTY_CART"
         })
     }
-    function setOrders (orders) {
+    function setOrders(orders) {
         dispatch({
-            type:"SET__ORDERS",
-            payload:orders
+            type: "SET__ORDERS",
+            payload: orders
         })
     }
-    console.log(state)
-    return <DataContext.Provider value = {{
-        products:state.products,
-        text:state.text, 
+    return <DataContext.Provider value={{
+        products: state.products,
+        text: state.text,
         setText,
         setUser,
-        user:state.user,
+        user: state.user,
         setUserToLs,
         setCartToLs,
         setOrders,
-        orderss:state.orderss,
-        shopingCart:state.shopingCart,
+        orderss: state.orderss,
+        shopingCart: state.shopingCart,
         emptyCart
-        }}>
+    }}>
         {props.children}
     </DataContext.Provider>
 }
 
-   
-
- 
 
 
 
- 
+
+
+
+
